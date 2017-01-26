4e9a4e2f4d3f550b92a7880821bf46491138333d
Dominic Mortlock <dom.mortlock@gmail.com>
2017-01-25 23:39:31 -0800

    created starter functions, and begun splitting input into tokens

--- src/MiniParser.java
+++ src/MiniParser.java
@@ -1,5 +1,106 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MiniParser
 {
+	private static final Map<String, String> TOKEN_MAP = createTokenMap();
+	private static final Map<String, String> KEYWORD_MAP = createKeywordMap();
 
+	private static Map<String, String> createTokenMap()
+	{
+		Map<String, String> map = new HashMap<String, String>();
+		map.put(" ", "SPACE");
+		map.put("\n", "NEWLINE");
+		map.put("{", "LEFT-BRACE");
+		map.put("}", "RIGHT-BRACE");
+		map.put("(", "LEFT-PARENTHESIS");
+		map.put(")", "RIGHT-PARENTHESIS");
+		map.put("=", "EQUALS");
+		map.put("!", "NOT");
+		map.put(";", "SEMICOLON");
+		return Collections.unmodifiableMap(map);
+	}
+
+	private static Map<String, String> createKeywordMap()
+	{
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("if", "KEYWORD-IF");
+		map.put("else", "KEYWORD-ELSE");
+		map.put("true", "KEYWORD-TRUE");
+		map.put("false", "KEYWORD-FALSE");
+		return Collections.unmodifiableMap(map);
+	}
+
+	public static boolean isLetter(char character)
+	{
+		return false;
+	}
+
+	public static List<String> getTokensFromFile(String inputPath) throws IOException
+	{
+		List<String> tokens = new ArrayList<String>();
+
+		try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(inputPath)))
+		{
+			for (String line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine())
+			{
+				char[] chars = line.toCharArray();
+				int loc = 0;
+
+				while (loc < line.length())
+				{
+					String token;
+					if ((token = TOKEN_MAP.get(chars[loc])) != null)
+					{
+						tokens.add(token);
+					}
+					else
+					{
+						String word = chars[loc] + "";
+						while (isLetter(chars[loc + 1]))
+						{
+							loc++;
+							word += chars[loc];
+						}
+						
+						if ((token = KEYWORD_MAP.get(word)) != null)
+						{
+							tokens.add(token);
+						}
+						else
+						{
+							tokens.add("IDENTIFIER(" + word + ")");
+						}
+					}
+					loc++;
+				}
+			}
+		}
+		return tokens;
+	}
+
+	public static boolean checkValidity(String inputPath) throws IOException
+	{	
+		List<String> tokens = getTokensFromFile(inputPath);
+		return false;
+	}
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			System.out.println(checkValidity(args[0]) ? "valid program" : "invalid program");
+		}
+		catch (IndexOutOfBoundsException | IOException e)
+		{
+			System.out.println("Please enter a valid input file as a command line parameter.");
+		}
+	}
 }
